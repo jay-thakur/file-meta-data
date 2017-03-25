2c3c6c3956394dc4a9692a92c7d756570c447d7b
Jay Prakash Thakur <jayprakashthakursnr@gmail.com>
2017-03-25 10:06:21 +0000

    file-meta-data impl

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var multer  = require('multer');
+
 var app = express();
+var upload = multer();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/api/getFileSize', upload.single('file'), function(req, res){
+    res.json({
+        size: req.file.size   
+    }); 
 });
 
 app.listen(process.env.PORT || 8080, function(){
